7c22aca7d4d8895bdd60c8dae3a51af6b9961f30
trey614 <Catdaddytrey@gmail.com>
2025-04-13 19:08:19 -0400

    Built an working display screen for our Alien Invasion game.

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,5 +1,24 @@
 import sys
 import pygame
-anyways
+#Making an class for our alien invasion game, where we can set function to display screen.
+class AlienInvasion:
+    def __init__(self):
+        pygame.init()
+    #The __init__ is us inheriting from our imported classs
+        self.screen = pygame.display.set_mode((1200,800))
+        pygame.display.set_caption("Alien Invasion")
+        self.running = True
+        #Here we are making our screen and setting its paremeter's
+    def run_game(self):
+        #Game Loop
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    pygame.quit()
+                    sys.exit()
+                    pygame.display.flip()
 if __name__ == '__main__':
-    pass
+    ai = AlienInvasion()
+    ai.run_game()
+    #We must set our class/ functions equal to someting so were able to run them.
