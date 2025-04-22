7949e85b5701e4f15005e253ecee76aaaa104a6b
trey614 <Catdaddytrey@gmail.com>
2025-04-21 23:12:18 -0400

    icrease difficulty function.

--- setting.py
+++ setting.py
@@ -8,10 +8,12 @@ class Settings:
         self.name: str = "Alien Invasion"         # Window title
         self.screen_w = 1200                      # Width of the game window
         self.screen_h = 800                       # Height of the game window
-        self.FPS = 60                             # Frames per second
+        self.FPS = 60      
+                           # Frames per second
 
         # Background settings
         self.bg_file = Path.cwd() / "Assets" / "images" / "Starbasesnow.png"  # Background image path
+        self.difficulty_scale = 1.1
 
         # Ship settings
         self.ship_file = Path.cwd() / "Assets" / "images" / 'ship2(no bg).png'  # Ship image path
@@ -33,8 +35,32 @@ class Settings:
 
         # Alien fleet settings
         self.alien_file = Path.cwd() / "Assets" / "images" / "enemy_4.png"  # Alien image path
-        self.fleet_speed = 3                         # Speed of the alien fleet's vertical movement
+        self.fleet_speed = 5                     # Speed of the alien fleet's vertical movement
         self.alien_w = 30                            # Alien width
         self.alien_h = 20                            # Alien height
         self.fleet_direction = 1                     # Fleet movement direction: 1 = down, -1 = up
         self.fleet_dropspeed = 50                    # Horizontal advancement when reaching top/bottom
+
+        self.button_w = 200
+        self.button_h = 50
+        self.button_color = (0,135,50)
+
+        self.text_color = (255,255,255)
+        self.button_font_size = 48
+        self.HUD_font_size = 20
+        self.font_file = Path.cwd() / "Assets" / "Fonts" / "Silkscreen" / "Silkscreen-Bold.ttf"
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 5
+        self.starting_ship_count = 3
+        self.bullet_speed = 7
+        self.bullet_amount = 5
+        self.fleet_speed = 2
+        self.fleet_drop_speed = 40
+        self.bullet_w = 25
+        self.bullet_h = 80
+
+    def increase_difficulty(self):
+        self.ship_speed *= self.difficulty_scale
+        self.bulett_speed *= self.difficulty_scale
+        self.fleet_speed *= self.difficulty_scale
